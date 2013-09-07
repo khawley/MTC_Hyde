e685d3ae03a1d876493c0d92e4ec2c66ef9beb76
Kelsey Hawley <k.karin.hawley@gmail.com>
2013-09-06 17:34:43 -0700

    removed images from git repository

--- content/images/Photos/Retreat Speakers/Bill Kees_files/KamU7KNCYdj.js
+++
@@ -1,5 +0,0 @@
-/*1343621413,169776321*/
-
-if (window.CavalryLogger) { CavalryLogger.start_js(["bs0gy"]); }
-
-__d("legacy:PhotoSnowlift",["PhotoSnowlift"],function(a,b,c,d){a.PhotoSnowlift=b('PhotoSnowlift');},3);
